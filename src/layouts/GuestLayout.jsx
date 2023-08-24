1af1057313f84603aa65b4040ec5377dedfd9479
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-08-24 09:20:24 -0300

    ajustar contenedor azul al alto de la pantalla en celulares

--- src/layouts/GuestLayout.jsx
+++ src/layouts/GuestLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 
 function GuestLayout() {
     return (
-        <div className="md:min-h-screen bg-gradient-to-bl from-grad-blue to-grad-green overflow-y-auto flex items-center justify-center">
+        <div className="min-h-screen bg-gradient-to-bl from-grad-blue to-grad-green overflow-y-auto flex items-center justify-center">
             <div className="p-6 flex items-center justify-center">
                 <div className="max-w-sm p-6 bg-white rounded-md shadow-lg w-80">
                 <Outlet />
